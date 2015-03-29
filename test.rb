f7001091bbbb94b7e67cb0244021f523a71ef5c4
buissonFabien <buissonfabien1@gmail.com>
2015-03-29 20:24:22 +0200

    test

--- test.rb
+++ test.rb
@@ -11,7 +11,7 @@ before do
    content_type :json
    headers 'Access-Control-Allow-Origin'  => '*', 
    		   'Access-Control-Allow-Methods' => ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
-   		   'Access-Control-Allow-Headers' => 'accept, origin, Content-Type : json'
+   		   # 'Access-Control-Allow-Headers' => 'accept, origin, Content-Type : json'
 end
 
 def get_connection
