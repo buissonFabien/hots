0d7aed1e3adcaa0e06def928e96fe43337083012
buissonFabien <buissonfabien1@gmail.com>
2015-03-31 14:25:04 +0200

    ok

--- test.rb
+++ test.rb
@@ -18,6 +18,9 @@ end
 
 def get_connection
   puts "ok"
+  puts db_user
+  puts db_password
+  
   return @db_connection if @db_connection
   db = URI.parse('mongodb://fab:fab@ds031601.mongolab.com:31601/')
   db_name = 'heroku_app35238099'
@@ -26,8 +29,6 @@ def get_connection
   @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
   @db_connection
   
-  puts db_password
-  puts db_user
 end
 
 
